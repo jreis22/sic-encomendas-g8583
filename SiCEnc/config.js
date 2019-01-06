00e671cf4bf7173d1c10a0e60cfc7df3f05972de
joao reis <1160600@isep.ipp.pt>
2019-01-06 18:58:39 +0000

    url fix again

--- SiCEnc/config.js
+++ SiCEnc/config.js
@@ -2,5 +2,5 @@ module.exports = {
 
     sicGcUrl: 'https://sicgc-g8583.azurewebsites.net/api/',
     sicEncDbUrl: process.env.SIC_ENC_DB_URL,
-    sicEntregasUrl: 'http://40.89.154.142:3001/'
+    sicEntregasUrl: '40.89.154.142:3001/'
 }
